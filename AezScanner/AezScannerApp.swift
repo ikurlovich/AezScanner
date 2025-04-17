f3764ea571b9b4c7f72fb14434fe298581fe958b
Ilia Kurlovich <ikurlovich@icloud.com>
2025-04-17 20:39:51 +0400

    Added an empty project body

---
+++ AezScanner/AezScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AezScannerApp.swift
+//  AezScanner
+//
+//  Created by Ilia Kurlovich on 17.04.25.
+//
+
+import SwiftUI
+
+@main
+struct AezScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
